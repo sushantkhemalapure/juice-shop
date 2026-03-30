7ac6be479678ef2998f6d511669843ebf450eabb
sushant khemalapure <sushantkhemalapure@gmail.com>
2026-03-30 19:47:51 +0530

    test(cypress): re-enable reflected XSS coverage in search
    Signed-off-by: sushant khemalapure <sushantkhemalapure@gmail.com>

--- test/cypress/e2e/trackOrder.spec.ts
+++ test/cypress/e2e/trackOrder.spec.ts
@@ -1,20 +1,16 @@
 describe('/#/track-order', () => {
   describe('challenge "reflectedXss"', () => {
-    // Cypress alert bug
-    xit('Order Id should be susceptible to reflected XSS attacks', () => {
+    it('Order Id should be susceptible to reflected XSS attacks', () => {
       cy.task('isDocker').then((isDocker) => {
         if (!isDocker) {
-          cy.on('uncaught:exception', (_err, _runnable) => {
-            return false
-          })
+          const xssPayload = '<iframe src="javascript:alert(`xss`)">'
 
           cy.visit('/#/track-result')
-          cy.visit('/#/track-result?id=<iframe src="javascript:alert(`xss`)">')
-          cy.reload()
+          cy.visit(`/#/track-result?id=${encodeURIComponent(xssPayload)}`)
 
-          cy.on('window:alert', (t) => {
-            expect(t).to.equal('xss')
-          })
+          cy.get('app-track-result h1 code iframe')
+            .should('exist')
+            .and('have.attr', 'src', 'javascript:alert(`xss`)')
 
           cy.expectChallengeSolved({ challenge: 'Reflected XSS' })
         }
